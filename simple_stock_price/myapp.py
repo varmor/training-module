e17e9240e98dd64336ed66ed5f82f59bc7dc466b
varunm <varunm@winsoftech.com>
2023-09-08 11:33:43 +0530

    adding project files

---
+++ simple_stock_price/myapp.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+
+st.write("""
+# Simple Stock Price App
+
+Shown are the stock closing price and volume of GOOGLE!
+
+""")
+
+# -*
+#define the ticker symbol
+tickerSymbol = 'GOOGL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+
+
+st.line_chart(tickerDf.Close)
+st.line_chart(tickerDf.Volume)
