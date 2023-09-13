4f24438ac13ac0ef516adc062723f8f5471efd47
varunm <varunm@winsoftech.com>
2023-09-13 12:47:24 +0530

    Completed simple bioinformatics dna project

--- 2_simple_bioinformatics_dna/dna-app.py
+++ 2_simple_bioinformatics_dna/dna-app.py
@@ -4,8 +4,7 @@ import altair as alt
 from PIL import Image
 
 
-# Page Title
-
+#Page logo
 image = Image.open('dna-logo.jpg')
 
 st.image(image, use_column_width=True)
@@ -21,70 +20,64 @@ This app counts the nucleotide composition of query DNA!
 
 
 # Input Text Box
-
-
-#st.sidebar.header('Enter DNA sequence')
 st.header('Enter DNA sequence')
 
 sequence_input = ">DNA Query 2\nGAACACGTGGAGGCAAACAGGAAGGTGAAGAAGAACTTATCCTATCAGGACGGAAGGTCCTGTGCTCGGG\nATCTTCCAGACGTCGCGACTCTAAATTGCCCCCTCTGAGGTCAAGGAACACAAGATGGTTTTGGAAATGC\nTGAACCCGATACATTATAACATCACCAGCATCGTGCCTGAAGCCATGCCTGCTGCCACCATGCCAGTCCT"
-
-#sequence = st.sidebar.text_area("Sequence input", sequence_input, height=250)
 sequence = st.text_area("Sequence input", sequence_input, height=250)
 sequence = sequence.splitlines()
-sequence = sequence[1:] # Skips the sequence name (first line)
-sequence = ''.join(sequence) # Concatenates list to string
+sequence = sequence[1:] 
+sequence = ''.join(sequence) 
+result = st.button(label="Click me")
 
-st.write("""
-***
-""")
+if result:
+  st.write("""
+  ***
+  """)
+ 
+  ## Prints the input DNA sequence
+  st.header('INPUT (DNA Query)')
+  sequence
 
-## Prints the input DNA sequence
-st.header('INPUT (DNA Query)')
-sequence
+  ## DNA nucleotide count
+  st.header('OUTPUT (DNA Nucleotide Count)')
 
-## DNA nucleotide count
-st.header('OUTPUT (DNA Nucleotide Count)')
+  ### 1. Print dictionary
+  st.subheader('1. Print dictionary')
+  def DNA_nucleotide_count(seq):
+    d = dict([
+              ('A',seq.count('A')),
+              ('T',seq.count('T')),
+              ('G',seq.count('G')),
+              ('C',seq.count('C'))
+              ])
+    return d
 
-### 1. Print dictionary
-st.subheader('1. Print dictionary')
-def DNA_nucleotide_count(seq):
-  d = dict([
-            ('A',seq.count('A')),
-            ('T',seq.count('T')),
-            ('G',seq.count('G')),
-            ('C',seq.count('C'))
-            ])
-  return d
+  X = DNA_nucleotide_count(sequence)
+  X
 
-X = DNA_nucleotide_count(sequence)
+  ### Print text
+  st.subheader('2. Print text')
+  st.write('There are  ' + str(X['A']) + ' adenine (A)')
+  st.write('There are  ' + str(X['T']) + ' thymine (T)')
+  st.write('There are  ' + str(X['G']) + ' guanine (G)')
+  st.write('There are  ' + str(X['C']) + ' cytosine (C)')
 
-#X_label = list(X)
-#X_values = list(X.values())
+  ### Display DataFrame
+  st.subheader('3. Display DataFrame')
+  df = pd.DataFrame.from_dict(X, orient='index')
+  # we have set orient = index because by default pd consider dict.keys as dataframe columns/
+  df = df.rename({0: 'count'}, axis='columns')
+  df.reset_index(inplace=True)
+  df = df.rename(columns = {'index':'nucleotide'})
+  st.write(df)
 
-X
-
-### Print text
-st.subheader('2. Print text')
-st.write('There are  ' + str(X['A']) + ' adenine (A)')
-st.write('There are  ' + str(X['T']) + ' thymine (T)')
-st.write('There are  ' + str(X['G']) + ' guanine (G)')
-st.write('There are  ' + str(X['C']) + ' cytosine (C)')
-
-### Display DataFrame
-st.subheader('3. Display DataFrame')
-df = pd.DataFrame.from_dict(X, orient='index')
-df = df.rename({0: 'count'}, axis='columns')
-df.reset_index(inplace=True)
-df = df.rename(columns = {'index':'nucleotide'})
-st.write(df)
-
-### Display Bar Chart using Altair
-st.subheader('4. Display Bar chart')
-p = alt.Chart(df).mark_bar().encode(
-    x='nucleotide',
-    y='count'
-)
-p = p.properties(
-    width=alt.Step(80)  # controls width of bar.
-)
-st.write(p)
+  ### Display Bar Chart using Altair
+  st.subheader('4. Display Bar chart')
+  p = alt.Chart(df).mark_bar().encode(
+      x='nucleotide',
+      y='count'
+  )
+  p = p.properties(
+      width=alt.Step(80)  # controls width of bar.
+  )
+  st.write(p)
